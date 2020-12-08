f72158d445bb496a2d5a09252e0b2623e017498d
Imtiaz A <ahmedimtiaz.ahmed@gmail.com>
2020-12-08 20:27:03 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main {
+	public static void main(String[] args) {
+		int income = 120_000;
+		String className = income > 100_000 ? "First" : "Economy" ;
+		// if (income > 100_000)
+		// 	className = "First";
+		// else
+		// className = "Economoy";
+
+		System.out.println(className);
+	}
+}
